73ce52a4ccd3cef1d3f49dcd9c3513dfc1d99d98
Landali <allanpaz93@hotmail.com>
2021-04-18 15:29:15 -0600

    add loader to diploma view, retrieve student list from lambda

--- student-manage-fe/src/services/getStudentbyId.js
+++ student-manage-fe/src/services/getStudentbyId.js
@@ -3,19 +3,18 @@
 import axios from 'axios'
 
 const getStudent = ({
-    firstname,
-    lastname,
+    ID,
     source
 }) => axios
     .get(`https://dvtbccva71.execute-api.us-east-1.amazonaws.com/dev/get-student/${ID}`, {
         cancelToken: source.token
     }).then((student) => {
         console.debug('Student retrieved successfully', student)
-        return Promise.resolve(student)
+        return Promise.resolve(student.data.student || {})
     })
     .catch((error) => {
-        console.error(`An error occured while retrieving student: ${firstname} ${lastname}`, error)
+        console.error(`An error occured while retrieving student with Id:${ID}`, error)
         return Promise.reject({})
     })
 
-    export default getStudent
+export default getStudent
