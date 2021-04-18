684030dc585e4500201019128d98e6c44f14929b
Landali <allanpaz93@hotmail.com>
2021-04-18 12:16:50 -0600

    add axios and create service for get and create student

--- student-manage-fe/src/services/createStudent.js
+++ student-manage-fe/src/services/createStudent.js
@@ -1,1 +1,24 @@
 //https://dvtbccva71.execute-api.us-east-1.amazonaws.com/dev/create-student/{firstname}
+import axios from 'axios'
+
+const createStudent = ({
+    firstname,
+    lastname,
+    source
+}) => axios
+    .post(`https://dvtbccva71.execute-api.us-east-1.amazonaws.com/dev/create-student/${firstname}`, {
+        cancelToken: source.token,
+        params: {
+            firstname,
+            lastname
+        }
+    }).then((student) => {
+        console.debug('Student was created successfully!', student)
+        return Promise.resolve(student)
+    })
+    .catch((error) => {
+        console.error(`An error occured while creating student: ${firstname} ${lastname}`, error)
+        return Promise.reject([])
+    })
+
+    export default createStudent
--- student-manage-fe/src/services/getStudentbyId.js
+++ student-manage-fe/src/services/getStudentbyId.js
@@ -1,1 +1,21 @@
 // Endpoint: https://dvtbccva71.execute-api.us-east-1.amazonaws.com/dev/get-student/{ID}
+
+import axios from 'axios'
+
+const getStudent = ({
+    firstname,
+    lastname,
+    source
+}) => axios
+    .get(`https://dvtbccva71.execute-api.us-east-1.amazonaws.com/dev/get-student/${ID}`, {
+        cancelToken: source.token
+    }).then((student) => {
+        console.debug('Student retrieved successfully', student)
+        return Promise.resolve(student)
+    })
+    .catch((error) => {
+        console.error(`An error occured while retrieving student: ${firstname} ${lastname}`, error)
+        return Promise.reject({})
+    })
+
+    export default getStudent
